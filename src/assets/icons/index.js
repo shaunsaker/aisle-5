9000a4ffb7c857e7111f900e949372695864d5a4
shaunsaker <shaun@aux.co.za>
2018-10-24 07:11:29 +0200

    Add custom icon font

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,8 +1,7 @@
 import createIconSet from 'react-native-vector-icons/lib/create-icon-set';
 
 const glyphMap = {
-  google: 59651,
-  facebook: 59659,
+  logo: 59648,
 };
 
 const Icon = createIconSet(glyphMap, 'AppIcons', 'AppIcons.ttf');
