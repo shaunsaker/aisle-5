cd4a754a2c101e18dcbcd94620c42bd9d09ed83a
shaunsaker <shaun@aux.co.za>
2018-10-24 07:28:20 +0200

    Add accent color

--- src/styleConstants/colors/index.js
+++ src/styleConstants/colors/index.js
@@ -1,6 +1,7 @@
 const colors = {
   primary: '#08293A',
   secondary: '#FEA116',
+  accent: '#D17623',
   white: '#EDE8CA',
   transWhite: 'rgba(273, 232, 202, 0.67)',
   transBlack: 'rgba(0, 0, 0, 0.67)',
