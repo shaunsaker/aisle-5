9e4930becee70bf55e8905a45d1dbb959c8eec97
shaunsaker <shaun@aux.co.za>
2018-10-24 07:19:16 +0200

    Add TitleText component

---
+++ src/components/TitleText/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text } from 'react-native';
+
+import styleConstants from '../../styleConstants';
+
+import styles from './styles';
+
+const propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+};
+
+const defaultProps = {
+  text: styleConstants.colors.white,
+};
+
+const TitleText = ({ text, color }) => {
+  return <Text style={[styles.text, { color }]}>{text}</Text>;
+};
+
+TitleText.propTypes = propTypes;
+TitleText.defaultProps = defaultProps;
+
+export default TitleText;
