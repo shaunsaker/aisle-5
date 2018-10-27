583f353637e76bcf43488658cc2f5467a5f0a193
shaunsaker <shaun@aux.co.za>
2018-10-27 09:19:59 +0200

    Fix getUniqueID service

--- src/services/deviceInfo/getUniqueID/index.js
+++ src/services/deviceInfo/getUniqueID/index.js
@@ -1,17 +1,11 @@
 import DeviceInfo from 'react-native-device-info';
 
-import utils from '../../../utils';
-
 export default function getUniqueID() {
-  return new Promise((resolve, reject) => {
-    DeviceInfo.getUniqueID()
-      .then((uniqueID) => {
-        resolve({
-          uniqueID,
-        });
-      })
-      .catch((error) => {
-        reject(utils.app.createError(error));
-      });
+  return new Promise((resolve) => {
+    const uniqueID = DeviceInfo.getUniqueID();
+
+    resolve({
+      uniqueID,
+    });
   });
 }
