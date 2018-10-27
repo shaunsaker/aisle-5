e37755c5060a0ce60d04530ab355be19e74bde02
shaunsaker <shaun@aux.co.za>
2018-10-27 13:44:17 +0200

    Add ItemSuggestionsList tests

---
+++ src/components/ItemSuggestionsList/__tests__/ItemSuggestionsList.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import ItemSuggestionsList from '..';
+import USER_ITEM from '../../../mockData/userItem';
+
+describe('ItemSuggestionsList', () => {
+  const spies = [];
+  const dispatch = jest.fn();
+  const data = [USER_ITEM];
+
+  describe('renders', () => {
+    it('renders with minimum required props', () => {
+      const component = renderer.create(<ItemSuggestionsList data={data} />);
+
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe('methods', () => {
+    it('should handle renderItem', () => {
+      // NOTE: We assume this works and is handled by RN FlatList
+    });
+  });
+
+  describe('actions', () => {
+    // NOTE: We assume simple prop actions work here
+  });
+
+  afterEach(() => {
+    spies.forEach((spy) => {
+      if (spy) {
+        spy.mockClear();
+      }
+    });
+    dispatch.mockClear();
+  });
+});
---
+++ src/mockData/userItem.js
@@ -0,0 +1,6 @@
+const userItem = {
+  name: 'Beer',
+  id: '1',
+};
+
+export default userItem;
