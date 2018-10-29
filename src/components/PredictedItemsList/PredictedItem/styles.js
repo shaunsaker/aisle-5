a98dfa8286dd1fe785cdc6411f601d9bb3634966
shaunsaker <shaun@aux.co.za>
2018-10-29 16:32:59 +0200

    Fix android Prediction items days left overlay

--- src/components/PredictedItemsList/PredictedItem/styles.js
+++ src/components/PredictedItemsList/PredictedItem/styles.js
@@ -24,6 +24,7 @@ const styles = StyleSheet.create({
     right: styleConstants.dimensions.spacing.horizontal,
     bottom: 0,
     justifyContent: 'center',
+    elevation: 100, // force it on top - android
   },
   daysLeftText: {
     ...styleConstants.fonts.types.small,
