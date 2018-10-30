b5b83d979088205cad347866c8df1edf10bbab6d
shaunsaker <shaun@aux.co.za>
2018-10-30 08:24:32 +0200

    Add ShoppingItemsList tests

---
+++ src/components/ShoppingItemsList/__tests__/ShoppingItemsList.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import ItemsList from '..';
+import ITEM from '../../../mockData/item';
+
+jest.mock('../ShoppingItem', () => 'ShoppingItem');
+jest.mock('../../ItemSeparator', () => 'ItemSeparator');
+
+// Fixes _bezier is not a function bug
+jest.useFakeTimers();
+
+describe('ItemsList', () => {
+  const data = [ITEM];
+
+  describe('renders', () => {
+    it('renders with minimum required props', () => {
+      const component = renderer.create(<ItemsList data={data} />);
+
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  // NOTE: We assume the actions are handled correctly
+});
