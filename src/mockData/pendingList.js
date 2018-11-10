c5a104eac43c6791135b37a1f6ce841941a2f750
shaunsaker <shaun@aux.co.za>
2018-11-10 09:27:16 +0200

    Add pending list to mockData

---
+++ src/mockData/pendingList.js
@@ -0,0 +1,58 @@
+const pendingList = {
+  1: {
+    name: 'Bread',
+    quantity: 2,
+    isChecked: true,
+    id: '1',
+  },
+  2: {
+    name: 'Cheese',
+    quantity: 1,
+    isChecked: true,
+    id: '2',
+  },
+  3: {
+    name: 'Chicken',
+    quantity: 1,
+    isChecked: true,
+    id: '3',
+  },
+  4: {
+    name: 'Apple',
+    quantity: 1,
+    isChecked: true,
+    id: '4',
+  },
+  5: {
+    name: 'Banana',
+    quantity: 1,
+    isChecked: false,
+    id: '5',
+  },
+  6: {
+    name: 'Pineapple',
+    quantity: 1,
+    isChecked: false,
+    id: '6',
+  },
+  7: {
+    name: 'Coffee',
+    quantity: 1,
+    isChecked: false,
+    id: '7',
+  },
+  8: {
+    name: 'Beer',
+    quantity: 6,
+    isChecked: false,
+    id: '8',
+  },
+  9: {
+    name: 'Cake',
+    quantity: 1,
+    isChecked: false,
+    id: '9',
+  },
+};
+
+export default pendingList;
