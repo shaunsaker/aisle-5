a8d677058ad2f16c5e7755e04c30ed739ce6033d
shaunsaker <shaun@aux.co.za>
2018-11-10 10:13:11 +0200

    Add shoppingList mock data

---
+++ src/mockData/shoppingList.js
@@ -0,0 +1,44 @@
+const shoppingList = [
+  {
+    id: '1',
+    name: 'Bread',
+    quantity: 2,
+  },
+  {
+    id: '2',
+    name: 'Cheese',
+    quantity: 1,
+  },
+  {
+    id: '3',
+    name: 'Chicken',
+    quantity: 0.5,
+  },
+  {
+    id: '4',
+    name: 'Apple',
+    quantity: 6,
+  },
+  {
+    id: '5',
+    name: 'Banana',
+    quantity: 4,
+  },
+  {
+    id: '6',
+    name: 'Pineapple',
+    quantity: 1,
+  },
+  {
+    id: '7',
+    name: 'Coffee',
+    quantity: 0.3,
+  },
+  {
+    id: '8',
+    name: 'Beer',
+    quantity: 6,
+  },
+];
+
+export default shoppingList;
