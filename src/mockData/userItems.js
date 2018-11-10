01c18d9c418ed7d2bf3c8b35b96fd4a43c2e1704
shaunsaker <shaun@aux.co.za>
2018-11-10 10:07:27 +0200

    Update userItems and userLists mock data

--- src/mockData/userItems.js
+++ src/mockData/userItems.js
@@ -1,7 +1,20 @@
-import userItem from './userItem';
-
 const userItems = {
-  1: userItem,
+  1: {
+    name: 'Bread',
+    id: '1',
+  },
+  2: {
+    name: 'Cheese',
+    id: '2',
+  },
+  3: {
+    name: 'Chicken',
+    id: '3',
+  },
+  4: {
+    name: 'Apple',
+    id: '4',
+  },
 };
 
 export default userItems;
